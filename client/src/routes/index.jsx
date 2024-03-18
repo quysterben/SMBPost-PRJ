826db46dfea4ba6d52f297de5c29fe7595737866
Hoang Sy Quy <kiaisoft.quy.hoang@gmail.com>
2024-03-18 10:43:47 +0700

    fix: docker

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,10 +1,12 @@
 import NotFound from '../pages/NotFound';
 import Dashboard from '../pages/Dashboard';
+import Login from '../pages/Login';
 
 import DefaultLayout from '../layouts/DefaultLayout';
 
 const publicRoutes = [
   { path: '/dashboard', component: Dashboard, layout: DefaultLayout },
+  { path: '/login', component: Login, layout: null },
   { path: '*', component: NotFound, layout: null }
 ];
 
